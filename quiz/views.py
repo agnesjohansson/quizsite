511213bfdb7cf36f6d4d7ecea0aa83ed449b7adc
agnesjohansson <agnes@decuria.se>
2015-08-06 10:10:39 +0200

    Created views

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def startpage (request):
+	return render (request, "quiz/startpage.html")
+
+def startpage (request):
+	return render (request, "quiz/quiz.html")
+
+def startpage (request):
+	return render (request, "quiz/question.html")
+
+def startpage (request):
+	return render (request, "quiz/completed.html")
